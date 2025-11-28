0f259385ac61f52abc05cfad4549fe51481c814d
Esteban <esteban.heschung@etudiant.univ-rennes.fr>
2025-11-28 11:36:12 +0100

    Début de prog des api.

--- app/src/main/java/com/example/netstatus/MainActivity.java
+++ app/src/main/java/com/example/netstatus/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +13,52 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static List<String> apis;
 
+    void displayAlert(String text){
+        Toast alert = Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT);
+        alert.show();
+    }
     void init(){
+        apis = new ArrayList<String>();
+        apis.add("https://discordstatus.com");
+        apis.add("https://www.githubstatus.com");
+        apis.add("https://www.githubstatus.com");
+        apis.add("https://www.cloudflarestatus.com");
+        apis.add("https://www.redditstatus.com");
+
+        for (String site:apis){
+            try {
+                URL url = new URL(site+"/api/v2/status.json");
+                URLConnection conn = url.openConnection();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                //le flux fait crash l'app pour une raison inconnue...
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    System.out.println("Allo");
+                }
+
+                reader.close();
+            } catch (MalformedURLException e) {
+                displayAlert("Mauvaise URL");
+            } catch (IOException e) {
+                displayAlert("Erreur de donnée pour un service");
+            }
+        }
+
         CardView service1 = findViewById(R.id.card_service_1); // doit être fait après le setContentView !
 
         service1.setOnClickListener(new View.OnClickListener() {
