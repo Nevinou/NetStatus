298f4e70dac3b5d9be5ba043a1fbf49e58f0e847
Esteban <esteban.heschung@etudiant.univ-rennes.fr>
2025-12-09 11:52:49 +0100

    Fix bugs et mise en place des status, couleurs...

--- app/src/main/java/com/example/netstatus/MainActivity.java
+++ app/src/main/java/com/example/netstatus/MainActivity.java
@@ -30,42 +30,74 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
-class LoadAPI implements Runnable{
+class LoadAPI implements Runnable{ //implements runnabe, redéfinition de run
     Activity activity;
-    LoadAPI(Activity activity){
+    LoadAPI(Activity activity){ //constructeur
         this.activity = activity;
     }
 
-    void createCard(JSONObject json){
+    void createCard(JSONObject json, String site){
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 String serviceName;
+                String status;
+                int color;
                 try {
-                    JSONObject data = json.getJSONObject("page");
-                    serviceName = data.getString("name");
+                    JSONObject data = json.getJSONObject("page"); //récupération json page
+                    JSONObject stat = json.getJSONObject("status"); //récupération du status json
+                    serviceName = data.getString("name"); //récupération valeur name
+
+                    switch (stat.getString("indicator")) {
+                        case "none":
+                            status = activity.getString(R.string.oper); //récupération du string du fichier xml
+                            color = activity.getColor(R.color.oper);
+                            break;
+                        case "minor":
+                            status = activity.getString(R.string.warn);
+                            color = activity.getColor(R.color.warn);
+                            break;
+                        case "major":
+                            status = activity.getString(R.string.broken);
+                            color = activity.getColor(R.color.broken);
+                            break;
+                        case "critical":
+                            status = activity.getString(R.string.hard_broken);
+                            color = activity.getColor(R.color.hard_broken);
+                            break;
+                        case "maintenance":
+                            status = activity.getString(R.string.disabled);
+                            color = activity.getColor(R.color.disabled);
+                            break;
+                        default:
+                            status = activity.getString(R.string.unknown);
+                            color = activity.getColor(R.color.unknown);
+                            break;
+                    }
                 } catch (JSONException e) {
-                    displayAlert("Erreur JSON, récupération de la page");
-                    return;
+                    displayAlert("Erreur JSON, récupération de la page"); //affichage alerte
+                    return; //on execute pas le reste
                 }
 
-                LinearLayout parent = activity.findViewById(R.id.main); // doit être fait après le setContentView !
-                Log.d("working",serviceName);
-                CardView card = (CardView) LayoutInflater.from(activity).inflate(R.layout.template_main,parent,false);
+                LinearLayout parent = activity.findViewById(R.id.main); // doit être fait après le setContentView ! Récupération du parent (layout)
+                //Log.d("working",serviceName);
+                CardView card = (CardView) LayoutInflater.from(activity).inflate(R.layout.template_main,parent,false); //on copie le layout card avec inflate.from et on définit le parent. False = ne pas parenter automatiquement
 
-                TextView titre = card.findViewById(R.id.titre);
-                titre.setText(serviceName);
-
+                TextView titre = card.findViewById(R.id.titre); //récupération du titre de la card
+                titre.setText(serviceName); //on définit un texte
+                TextView serviceStatus = card.findViewById(R.id.status);
+                serviceStatus.setText(activity.getText(R.string.string_status)+" "+status); //on concatène "status :" avec le stauts actuel
+                serviceStatus.setTextColor(color);
 
                 card.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Intent intent = new Intent(activity,DetailsActivity.class);
-                        intent.putExtra("Name",serviceName);
-                        activity.startActivity(intent);
+                        Intent intent = new Intent(activity,DetailsActivity.class); //création d'un itent vers Details
+                        intent.putExtra("LienAPI",site); //on met un extra de donnée
+                        activity.startActivity(intent); //on affiche l'activity
                     }
                 });
-                parent.addView(card);
+                parent.addView(card); //parentage pour affichage
             }
         });
     }
@@ -75,7 +107,7 @@ class LoadAPI implements Runnable{
             @Override
             public void run() {
                 Toast alert = Toast.makeText(activity,text,Toast.LENGTH_SHORT); //Activity hérite de context
-                alert.show();
+                alert.show(); //on affiche le Toast
             }
         });
     }
@@ -83,26 +115,31 @@ class LoadAPI implements Runnable{
     @Override
     public void run() {
         for (String site : MainActivity.apis) {
-            try {
-                URL url = new URL(site + "/api/v2/status.json");
-                URLConnection conn = url.openConnection(); //ajouter la permission internet dans le manifest !
-                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                String total = "";
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    total += line;
+            new Thread(new Runnable() { // on fait dans un nouveau thread les requetes pour aller plus vite
+                @Override
+                public void run() {
+                    try {
+                        URL url = new URL(site + "/api/v2/status.json"); //création url
+                        URLConnection conn = url.openConnection(); //ajouter la permission internet dans le manifest ! Ouverture de la connexion
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream())); // Flux de lecture
+                        String total = "";
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            total += line; //lecture de toute les lignes
+                        }
+                        reader.close();
+                        //Log.d("JSON",total);
+                        JSONObject json = new JSONObject(total); //création d'un JSONObject
+                        createCard(json,site); //appel methode
+                    } catch (MalformedURLException e) {
+                        displayAlert("Mauvaise URL");
+                    } catch (JSONException e) {
+                        displayAlert("Erreur JSON");
+                    } catch (IOException e) {
+                        displayAlert("Erreur de donnée pour un service");
+                    }
                 }
-                reader.close();
-                Log.d("JSON",total);
-                JSONObject json = new JSONObject(total);
-                createCard(json);
-            } catch (MalformedURLException e) {
-                displayAlert("Mauvaise URL");
-            } catch (JSONException e) {
-                displayAlert("Erreur JSON");
-            } catch (IOException e) {
-                displayAlert("Erreur de donnée pour un service");
-            }
+            }).start();
         }
     }
 }
@@ -135,17 +172,17 @@ public class MainActivity extends AppCompatActivity {
         apis.add("https://alerts.library.nyu.edu");
         apis.add("https://status.developer.intuit.com");
         apis.add("https://status.epicgames.com");
-        apis.add("https://status.scaleway.com");
+        apis.add("https://status.scaleway.com"); //ajout des urls qui utilisent statuspage.io
 
         Thread thread = new Thread(new LoadAPI(this)); //le réseau doit être fait dans un thread externe
         thread.start();
 
-        Button more = findViewById(R.id.more);
+        Button more = findViewById(R.id.more); //on prend le boutton de l'activity qui a l'id "more"
         more.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,AllServicesActivity.class);
-                startActivity(intent);
+                Intent intent = new Intent(MainActivity.this,AllServicesActivity.class); //on fait un itent pour changer de page
+                startActivity(intent); //on change d'activity
             }
         });
     }
@@ -155,13 +192,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //rend visible
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left+20, systemBars.top, systemBars.right+20, systemBars.bottom);
             return insets;
         });
 
-        init();
+        init(); //appel de la méthode init
     }
 }
