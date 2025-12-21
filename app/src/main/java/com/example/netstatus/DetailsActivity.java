c3f9ec31fcd7293d4d6bf4fdfb9068ed5712e790
LOUËR Neven <neyhou@christophelouer.fr>
2025-12-21 21:17:29 +0100

    Refonte du disgne de la page activity_details.xml Automatisation du status Global commencé

--- app/src/main/java/com/example/netstatus/DetailsActivity.java
+++ app/src/main/java/com/example/netstatus/DetailsActivity.java
@@ -2,6 +2,7 @@ package com.example.netstatus;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -29,6 +30,10 @@ public class DetailsActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        String urlApi = datas.getStringExtra("LienAPI");
+        Thread thread = new Thread(new LoadApiDetail(this,urlApi));
+        thread.start();
 
     }
 
